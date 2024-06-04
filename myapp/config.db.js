7ee8b18678acc8e6dd6bd526c654791a36779ad4
Yiinn38 <rasec.romero46@gmail.com>
2024-06-04 00:37:06 -0600

    node_modules

---
+++ myapp/config.db.js
@@ -0,0 +1,20 @@
+
+const dotenv = require('dotenv');
+dotenv.config();
+const mysql = require('mysql');
+
+let connection
+
+try{
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    });
+}
+catch (error){
+    console.log(error);
+}
+
+module.exports = { connection };
--- myapp/index.js
+++ myapp/index.js
@@ -1,43 +1,17 @@
+
 let nombre = "Hola Mundo";
 console.log(nombre);
+
 const express = require('express');
-
 const app = express();
-
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hola Mundo!');
-});
+app.use(cors());
 
-app.get('/productos', (req, res) => {
-    let productos = [
-        {
-            nombre: 'Refresco',
-            precio: 17
-        },
-        {
-            nombre: 'Papas',
-            precio: 20
-        },
-        {
-            nombre: 'Chocolates',
-            precio: 25
-        },
-        {
-            nombre: 'Kit Kat',
-            precio: 30
-        }
-    ];
+app.use(express.json());
+app.use(express.urlencoded({extended : true}))
 
-    let respuesta = '<table border="1">';
-    productos.forEach(producto => {
-        respuesta += `<tr><td>${producto.nombre}</td><td>${producto.precio}</td></tr>`;
-    });
-    respuesta += '</table>';
-
-    res.send(respuesta);
-});
+app.use(require('./routes/productos'));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
